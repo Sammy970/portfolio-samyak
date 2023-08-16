4b8fc627fe2847af550f05d26443490b3348d743
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-08-16 16:38:53 +0530

    Made ProfileBar Component - Responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Profilebar from "./components/Profilebar/Profilebar";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Profilebar />
+    </div>
+  );
 }
 
 export default App;
