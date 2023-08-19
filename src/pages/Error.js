5f584f1b792d1e68b5cd416535a838395d2dd53c
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-08-19 18:49:56 +0530

    Fixed Error Page for all the routes - Mobile and Desktop Responsive

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import Profilebar from "../components/Profilebar/Profilebar";
-import { Grid, GridItem } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import Sidebar from "../components/Sidebar/Sidebar";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 const ErrorPage = () => {
   const error = useRouteError();
@@ -12,20 +22,66 @@ const ErrorPage = () => {
     <>
       <Profilebar />
       <main>
-        <Grid templateColumns="230px auto auto" gap={5}>
-          <GridItem
-            colSpan={1}
-            bg="#f5f5f5"
-            boxShadow="md"
-            h="70vh"
-            borderRadius="10px"
-          >
-            <Sidebar />
-          </GridItem>
-          <GridItem colSpan={2} boxShadow="md" borderRadius="10px">
-            {error.internal ? error.data: error.data.message}
-          </GridItem>
-        </Grid>
+        <Box display={{ base: "none", md: "block" }}>
+          <Grid templateColumns={"230px auto auto"} gap={5}>
+            <GridItem colSpan={1} maxH={"70vh"}>
+              <Sidebar />
+            </GridItem>
+            <GridItem colSpan={2} boxShadow="md" borderRadius="10px">
+              {error.internal ? error.data : error.data.message}
+            </GridItem>
+          </Grid>
+        </Box>
+
+        {/* For smaller screens (mobile) */}
+        <Box display={{ base: "block", md: "none" }} w={"full"}>
+          <Grid templateRows={"1fr auto"} gap={5} w={"full"}>
+            <GridItem
+              rowSpan={1}
+              // bg="#f5f5f5"
+              boxShadow="md"
+              h="auto"
+              w={"full"}
+              borderRadius="10px"
+            >
+              <Menu>
+                <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                  Menu
+                </MenuButton>
+                <MenuList
+                  color={"#1f1f1f"}
+                  fontFamily={"google-sans-medium"}
+                  fontSize={16}
+                >
+                  <Link to={"/"}>
+                    <MenuItem>About</MenuItem>
+                  </Link>
+                  <Link to={"/projects"}>
+                    <MenuItem>Projects</MenuItem>
+                  </Link>
+                  <Link to={"/blogs"}>
+                    <MenuItem>Blogs</MenuItem>
+                  </Link>
+                  <Link to={"/skills"}>
+                    <MenuItem>Skills</MenuItem>
+                  </Link>
+                  <Link to={"/certificates"}>
+                    <MenuItem>Certificates</MenuItem>
+                  </Link>
+                  <Link to={"/timeline"}>
+                    <MenuItem>Timeline</MenuItem>
+                  </Link>
+                  <Link to={"/contacts"}>
+                    <MenuItem>Contacts</MenuItem>
+                  </Link>
+                </MenuList>
+              </Menu>
+            </GridItem>
+            <GridItem rowSpan={1} boxShadow="md" borderRadius="10px">
+              {error.internal ? error.data : error.data.message}
+            </GridItem>
+          </Grid>
+        </Box>
       </main>
     </>
   );
