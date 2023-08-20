a0dacc82d28ea76a4155abac4fe94e0726c0ed3f
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-08-20 14:18:33 +0530

    Added Timeline data in a Array and displayed it - For both desktop and mobile - responsive

--- src/components/TimelineCard/TimelineCard.js
+++ src/components/TimelineCard/TimelineCard.js
@@ -2,15 +2,16 @@ import React from "react";
 
 import { Card, CardBody, Text } from "@chakra-ui/react";
 
-const TimelineCard = () => {
+const TimelineCard = (props) => {
+  const { content, color } = props;
   return (
-    <Card w={"full"} bg={"#FFFFFF"} p={3} borderRadius={7} mb={3}>
+    <Card w={"full"} bg={color} p={3} borderRadius={7} mb={3}>
       <CardBody
         color={"#1F1F1F"}
         fontFamily={"google-sans-black"}
-        fontSize={19}
+        fontSize={{ base: 16, sm: 17, lg: 18 }}
       >
-        <Text>Hello this is test</Text>
+        <Text>{content}</Text>
       </CardBody>
     </Card>
   );
--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -10,8 +10,62 @@ import {
   TimelineSeparator,
   timelineOppositeContentClasses,
 } from "@mui/lab";
+
 import TimelineCard from "../components/TimelineCard/TimelineCard";
+
 import { Text } from "@chakra-ui/react";
+
+const colorPalette = [
+  "#DDEFE0",
+  "#F4ECDD",
+  "#EFDADA",
+  "#DEE0EF",
+  // Add more colors if needed
+];
+
+const TimelineData = [
+  {
+    year: "2023",
+    data: [
+      "Built my own portfolio website from scratch",
+      "Present my Paper in IEEE Conference",
+      "Learnt ReactJS Basics by META",
+      "Enrolled in multiple Coursera courses for “Front-End Web Development",
+      "Wrote my First Technical Blog on Medium.com",
+    ],
+  },
+  {
+    year: "2022",
+    data: [
+      "Volunteered as a Database Co-ordinator for ISA (International Society of Automation).",
+      "Participated in a social event Utkarsh under VIT SOCIAL and WELFARE Department",
+    ],
+  },
+  {
+    year: "2021",
+    data: [
+      "Participated in State Level Project Competition (TECHNOMELA - 2021)",
+      "Secured First Rank in Business Ideathon (Polyspark 2021)",
+      "Ranked first again in my department in college",
+    ],
+  },
+  {
+    year: "2020",
+    data: [
+      "Winner in 9th Techno-Gurupoornima Online Project Competition",
+      "Participated in pre-qualifier round of International Space App Challenge",
+      "Participated in State Level Technical Paper Presentation (Pravinya 2020)",
+    ],
+  },
+  {
+    year: "2019",
+    data: [
+      "Ranked first in my department in college",
+      "Runner up in State Level Technical Paper Presentation (Technocratz 2019)",
+      "Secured third rank in Sketch Competition in Polyspark-2019",
+    ],
+  },
+];
 
 const TimelinePage = () => {
   return (
@@ -23,89 +77,33 @@ const TimelinePage = () => {
         },
       }}
     >
-      <TimelineItem>
-        <TimelineOppositeContent
-          fontFamily={"google-sans-medium"}
-          color={"#FFFFFF"}
-          alignSelf={"center"}
-          fontSize={18}
-        >
-          <Text textAlign={"center"}>2019</Text>
-        </TimelineOppositeContent>
+      {TimelineData.map((item) => (
+        <TimelineItem key={item.year}>
+          <TimelineOppositeContent
+            fontFamily={"google-sans-medium"}
+            color={"#FFFFFF"}
+            alignSelf={"center"}
+            fontSize={18}
+          >
+            <Text textAlign={"center"}>{item.year}</Text>
+          </TimelineOppositeContent>
 
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
+          <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+          </TimelineSeparator>
 
-        <TimelineContent>
-          <TimelineCard />
-          <TimelineCard />
-        </TimelineContent>
-      </TimelineItem>
-
-      <TimelineItem>
-        <TimelineOppositeContent
-          fontFamily={"google-sans-medium"}
-          color={"#FFFFFF"}
-          alignSelf={"center"}
-          fontSize={18}
-        >
-          <Text textAlign={"center"}>2020</Text>
-        </TimelineOppositeContent>
-
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-
-        <TimelineContent>
-          <TimelineCard />
-          <TimelineCard />
-        </TimelineContent>
-      </TimelineItem>
-
-      <TimelineItem>
-        <TimelineOppositeContent
-          fontFamily={"google-sans-medium"}
-          color={"#FFFFFF"}
-          alignSelf={"center"}
-          fontSize={18}
-        >
-          <Text textAlign={"center"}>2021</Text>
-        </TimelineOppositeContent>
-
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-
-        <TimelineContent>
-          <TimelineCard />
-          <TimelineCard />
-        </TimelineContent>
-      </TimelineItem>
-
-      <TimelineItem>
-        <TimelineOppositeContent
-          fontFamily={"google-sans-medium"}
-          color={"#FFFFFF"}
-          alignSelf={"center"}
-          fontSize={18}
-        >
-          <Text textAlign={"center"}>2022</Text>
-        </TimelineOppositeContent>
-
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-
-        <TimelineContent>
-          <TimelineCard />
-          <TimelineCard />
-        </TimelineContent>
-      </TimelineItem>
+          <TimelineContent>
+            {item.data.map((event, index) => (
+              <TimelineCard
+                key={index}
+                content={event}
+                color={colorPalette[index % colorPalette.length]}
+              />
+            ))}
+          </TimelineContent>
+        </TimelineItem>
+      ))}
     </Timeline>
   );
 };
