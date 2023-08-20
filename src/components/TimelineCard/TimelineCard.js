5f74e7b289dc86f3ef22a6bc1b821b437079d3a9
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-08-20 11:38:14 +0530

    Figured out how to make the timeline

--- src/components/TimelineCard/TimelineCard.js
+++ src/components/TimelineCard/TimelineCard.js
@@ -4,7 +4,7 @@ import { Card, CardBody, Text } from "@chakra-ui/react";
 
 const TimelineCard = () => {
   return (
-    <Card w={"full"} bg={"#FFFFFF"} p={3} borderRadius={7}>
+    <Card w={"full"} bg={"#FFFFFF"} p={3} borderRadius={7} mb={3}>
       <CardBody
         color={"#1F1F1F"}
         fontFamily={"google-sans-black"}
--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -6,20 +6,60 @@ import {
   TimelineContent,
   TimelineDot,
   TimelineItem,
+  TimelineOppositeContent,
   TimelineSeparator,
+  timelineOppositeContentClasses,
 } from "@mui/lab";
 import TimelineCard from "../components/TimelineCard/TimelineCard";
+import { Text } from "@chakra-ui/react";
 
 const TimelinePage = () => {
   return (
-    <Timeline position="alternate">
+    <Timeline
+      position="right"
+      sx={{
+        [`& .${timelineOppositeContentClasses.root}`]: {
+          flex: 0.1,
+        },
+      }}
+    >
       <TimelineItem>
+        <TimelineOppositeContent
+          fontFamily={"google-sans-medium"}
+          color={"#FFFFFF"}
+          fontSize={18}
+        >
+          <Text textAlign="center">2019</Text>
+        </TimelineOppositeContent>
+
         <TimelineSeparator>
           <TimelineDot />
           <TimelineConnector />
         </TimelineSeparator>
+
         <TimelineContent>
-          <TimelineCard title={"hel"} />
+          <TimelineCard />
+          <TimelineCard />
+        </TimelineContent>
+      </TimelineItem>
+
+      <TimelineItem>
+        <TimelineOppositeContent
+          fontFamily={"google-sans-medium"}
+          color={"#FFFFFF"}
+          fontSize={18}
+        >
+          <Text textAlign="center">2020</Text>
+        </TimelineOppositeContent>
+
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+
+        <TimelineContent>
+          <TimelineCard />
+          <TimelineCard />
         </TimelineContent>
       </TimelineItem>
     </Timeline>
